fd8f7d137a909222ffd97bcf0d2953dac88837dc
Jofre <aejofre@gmail.com>
2020-05-19 20:12:48 -0500

    referencia empresas

---
+++ src/containers/referencias-empresa/Tarjeta/Tarjeta.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Tarjeta = () => {
+
+    
+    return (
+        <div>
+            <div className="ofertas">
+                <div className="caja">
+                    <div className="card tarjeta-vacante">
+                        <div className="card-body">
+                            <div className="primera-mitad">
+                                <div className="main-info">
+                                    <div className="main-puntos col-9">
+                                        <div className="titulo-puesto">
+                                            <h5>Diseñador UI/ UX</h5>
+                                        </div>
+                                        <div className="subs-compañia">
+                                            <div className="nombre-compañia">
+                                                <p>Nearsoft</p>
+                                            </div>
+                                            <div className="duracion">
+                                                <p>Tiempo Completo</p>
+                                            </div>
+                                            <div className="ubicacion">
+                                                <p>Remoto</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="main-contador">
+                                        <div className="contador">
+
+                                        </div>
+                                        <div className="compensacion">
+                                            <span>Por Definir</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="segunda-mitad">
+                                <div className="caja-descripcion">
+                                    <p>Importante agencia de Marketing está en búsqueda de Diseñador UI/ UX (Sketch
+                                    APP)
+                                            para laborar en Corporativo en Reforma.</p>
+                                </div>
+                                <div className="habilidades">
+                                    <p>PHP</p>
+                                    <p>Python</p>
+                                    <p>HTML/CSS</p>
+                                    <p>Java Script</p>
+                                    <p>Git</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tarjeta
---
+++ src/containers/referencias-empresa/referencias-empresa.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Tarjeta from './Tarjeta/Tarjeta'
+
+const referenciasEmpresa = () => {
+    return (
+        <div>
+            <div>
+            <Filtro />
+            </div>
+
+            <div>
+            <Tarjeta />
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default referenciasEmpresa
